c581dbbb995ffeffbf96f608f60d72a7c2c4f0d3
SafychCSS <safaraliev.renat@yandex.ru>
2021-01-25 18:56:47 +0300

    btn disabled/enabled

--- src/components/NewNote.vue
+++ src/components/NewNote.vue
@@ -24,7 +24,15 @@
                 <label class="block text-xl" for="note-description">Description</label>
                 <textarea v-model="note.description" class="form__textarea block border w-full rounded-lg p-4 focus:border-purple-400 transition duration-300" name="note-description" id="note-description" cols="30" rows="4" placeholder="Description note" maxlength="200"></textarea>
             </div>
-            <button @click.prevent="addNote" class="flex mx-auto bg-purple-600 inline-block text-white px-7 py-4 rounded-full text-xl hover:shadow-xl transition-shadow duration-300">Add new</button>
+
+            <button
+                @click.prevent="addNote"
+                :disabled="note.title.length < 3"
+                :class="{'cursor-not-allowed opacity-50': note.title.length < 3 }"
+                class="flex mx-auto bg-purple-600 inline-block text-white px-7 py-4 rounded-full text-xl hover:shadow-xl transition-shadow duration-300"
+            >
+              Add new
+            </button>
         </form>
     </div>
 </template>
